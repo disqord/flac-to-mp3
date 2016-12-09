052df383bbaac9436b7816b330f3b1688dfc2253
disqord <areinartz89@gmail.com>
2016-12-09 15:51:48 +0100

    Update
    Removed debug echos
    Added shebang

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,26 +1,21 @@
-
+#!/usr/bin/env bash
 
 
 # take current dir name
-echo ${PWD##*/}
 STR=${PWD##*/}
-echo "${STR/FLAC/MP3}"
 
 # construct mp3 folder name
 # if it contains "FLAC" then substitute for "MP3[320]"
 # else append "MP3[320]"
 if [[ $STR == *"FLAC"* ]]
 then
- 	echo "It's there!";
  	MPMAP=${STR/FLAC/MP3[320]};
 else
-	echo "Itsnotdere";
-	MPMAP=$STR"- MP3[320]"
+	MPMAP=$STR"- MP3[320]";
 fi
 
 
 # make the folder
-echo $MPMAP;
 mkdir "$MPMAP";
 
 # recode in folder:
